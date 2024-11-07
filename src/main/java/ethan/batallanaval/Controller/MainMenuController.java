d9ac6248aa83422e517c7b99e5666b13d7997367
karsvolsv <ethannicolasw@gmail.com>
2024-11-06 23:12:25 -0500

    Menu principal

--- src/main/java/ethan/batallanaval/Controller/MainMenuController.java
+++ src/main/java/ethan/batallanaval/Controller/MainMenuController.java
@@ -1,15 +1,28 @@
 package ethan.batallanaval.Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class MainMenuController {
 
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    private void onHelloButtonClick() {
-        welcomeText.setText("¡Hola, bienvenido a Batalla Naval!");
+    private Button jugarButton;
+
+    @FXML
+    private Button salirButton;
+
+    @FXML
+    private void onJugarButtonClick() {
+    }
+
+    @FXML
+    private void onSalirButtonClick() {
+        Stage stage = (Stage) salirButton.getScene().getWindow();
+        stage.close();
     }
 }
+
+
